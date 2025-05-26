bfea1e4ad861d79a50b9b337d105bfd81e37f251
AllanMatiass <allan.gmp@puccampinas.edu.br>
2025-05-26 09:30:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Estrutura de dados
-voos = {}  # chave: número do voo, valor: dicionário com informações do voo
-passageiros = {}  # chave: CPF do passageiro, valor: dicionário com nome e telefone
+# Estrutura de dados 
+voos = {}  # chave: número do voo, valor: lista com informações do voo
+passageiros = {}  # chave: CPF do passageiro, valor: lista com nome e telefone
 voos_passageiros = {}  # chave: número do voo, valor: lista de CPFs
 
 # Função para cadastrar um novo voo
@@ -17,14 +17,6 @@ def cadastrar_voo():
     preco = float(input("Digite o preço da passagem: "))
     lugares = int(input("Digite a quantidade de lugares disponíveis: "))
 
-    # voos[numero] = {
-    #     "origem": origem,
-    #     "destino": destino,
-    #     "escalas": escalas,
-    #     "preco": preco,
-    #     "lugares": lugares
-    # }
-
     voos[numero] = [origem, destino, escalas, preco, lugares]
 
 
@@ -36,7 +28,7 @@ def consultar_voo():
     codigo = input('Digite o código do voo: ')
 
     while codigo not in voos:
-        print('digite um codigo existente ou digite "quit" para sair')
+        print('digite um codigo existente ou digite "quit" para cancelar a operação')
         codigo = input('Digite o código do voo: ')
 
         if codigo.strip().lower() == 'quit':
@@ -47,26 +39,84 @@ def consultar_voo():
     
     print(f'Origem: {voo[0]}\nDestino: {voo[1]}\nNumero de escalas: {voo[2]}\nPreço da passagem: {voo[3]}\nQuantidade de lugares disponíveis: {voo[4]}')
 
-def menu():
+def listar_voos():
+    if len(voos) > 0:
+        return
+    for i in voos:
+        cidade_embarque = voos[i][0]
+        destino = voos[i][1]
+        escalas = voos[i][2]
+        preco = voos[i][3]
+        lugares_disponiveis = voos[i][4]
+        print('-'*30)
+        print(f'VOO #{i}\nCidade de embarque: {cidade_embarque}\nDestino: {destino}\nEscalas: {escalas}\nPreço: R${preco}\nLugares disponíveis: {lugares_disponiveis}')
+        print('-'*30)
 
+def cadastrar_passageiro(cpf):
+    nome = input('Digite seu nome: ')
+    telefone = input('Digite seu telefone: ')
+    passageiros[cpf] = [nome, telefone]
+    
+
+def venda_passagem():
+    listar_voos()
+    voo_selecionado = input('Digite o ID do voo que deseja: ')
+
+    if voo_selecionado not in voos:
+        print('Digite um ID valido')
+        return
+    
+    voo = voos_passageiros[voo_selecionado]
+    cpf = input('Digite seu CPF: ')
+
+    if cpf in passageiros:
+        if cpf not in voo:
+            print('já existe em passageiros, usando informações existentes...')
+            voo.append([passageiros[cpf][0], passageiros[cpf][1]])
+            voos[voo_selecionado][4] -= 1 # Quantidade de lugares
+            print('Usuário cadastrado com sucesso')
+            return
+        
+        print('Usuario já cadastrado no voo.')
+        return
+    
+    cadastrar_passageiro(cpf)
+    voo.append([passageiros[cpf][0], passageiros[cpf][1]])
+    voos[voo_selecionado][4] -= 1 
+
+    
+def mostrar_opcoes():
     opcoes = {
         '1': [cadastrar_voo, 'Cadastrar voo'],
-        '2': [consultar_voo, 'Consultar voo']
+        '2': [consultar_voo, 'Consultar voo'],
+        '3': [venda_passagem, 'Vender passagem'],
+        '4': ['cancelar_passagem', 'Cancelar passagem'],
+        '5': [listar_voos, 'Listar voos'],
+        '6': ['informar_voo_menor_escala', 'Informar o voo com menor escala'],
+        '7': ['listar_passageiros_voo', 'Listar passageiros de um voo'],
+        '8': [None, 'Sair']
     }
     for i in opcoes:
         print(f'{i} - {opcoes[i][1]}')
-
-    escolha = input('Digite o numero do item que quer fazer: ')
-    if escolha not in opcoes:
-        print('Dá não')
-        return
     
-    opcoes[escolha][0]()
+    return opcoes
+    
+
+def menu():
+    opcoes = mostrar_opcoes()
+    escolha = input('Digite o numero da operação que deseja realizar: ')
+    
+    while escolha != '8':
+        
+        if escolha not in opcoes:
+            print('Escolha inválida')
+        else:
+            opcoes[escolha][0]()
+
+        opcoes = mostrar_opcoes()
+        escolha = input('Digite o numero da operação que deseja realizar: ')
+
 
 menu()
-print(voos)
-print(voos_passageiros)
+print(voos_passageiros, 'DEBUGGGGGGGGGGG')
 print('-'*40)
-
-
-
